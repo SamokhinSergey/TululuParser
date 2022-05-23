74adb3d068ca4f9cef4bf1d93dc01b1904f52ffb
SERGEY SAMOKHIN <samokhin@live.ru>
2022-05-23 15:15:11 +0400

    download books with titles.

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
 import requests
 import os
+from bs4 import BeautifulSoup
+from pathvalidate import sanitize_filename
+
+
+def download_txt(url, filename, folder='books/'):
+    """Функция для скачивания текстовых файлов.
+    Args:
+        url (str): Cсылка на текст, который хочется скачать.
+        filename (str): Имя файла, с которым сохранять.
+        folder (str): Папка, куда сохранять.
+    Returns:
+        str: Путь до файла, куда сохранён текст.
+    """
+    filename = sanitize_filename(filename)
+    folder = sanitize_filename(folder)
+    payload = {"id": book_id}
+    response = requests.get(url, payload, allow_redirects=False)
+    check_for_redirect(response)
+    response.raise_for_status()
+    file_patch = os.path.join(folder, filename + '.txt')
+    with open(file_patch, 'wb') as file:
+        file.write(response.content)
+    return file_patch
 
 
 def check_books_dir():
@@ -9,10 +32,31 @@ def check_books_dir():
     return books_dir
 
 
-for id in range(10):
-    url = f"https://tululu.org/txt.php?id={id}"
+def check_for_redirect(tululu_response):
+    if tululu_response.is_redirect:
+        raise requests.HTTPError
+
+
+def book_name(book_id):
+    url = f"https://tululu.org/b{book_id}"
     response = requests.get(url)
     response.raise_for_status()
-    filename = f'{check_books_dir()}/id{id}.txt'
-    with open(filename, 'wb') as file:
-        file.write(response.content)
+    soup = BeautifulSoup(response.text, 'lxml')
+    title_tag = soup.find(class_='ow_px_td').find('h1')
+    book_name = title_tag.text.split(':')[0].strip()
+    book_autror = title_tag.text.split(':')[2].strip()
+    print(book_name, book_autror)
+    return book_name
+
+
+for book_id in range(10):
+
+    try:
+        url = "https://tululu.org/txt.php"
+        download_txt(url, book_name(book_id), check_books_dir())
+
+    except requests.HTTPError:
+        print(f"Книги с id {book_id} не существует на сайте")
+
+    except:
+        print("1")
